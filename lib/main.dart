7298a79802be8085d7f3b4a91f996b3671bb3654
Tobias Oetiker <tobi@oetiker.ch>
2019-06-24 00:09:02 +0200

    more beautification

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ void main() {
 
   runApp(new MaterialApp(
     home: new TempAar(),
-    title: 'Aare Temperature',
+    title: 'River Aare in Olten',
     theme: ThemeData(
         primarySwatch: Colors.blue,
     ),
@@ -76,13 +76,15 @@ class TempAarState extends State<TempAar> {
       // refresh
       setState(() {});
     });
-    const double iconSize = 30;
+    const double iconSize = 54;
     const double titleScale = 0.8;
-    const double subtitleScale = 1.8;
+    const double subtitleScale = 2;
+    const Color iconColor = Colors.black12;
 
     return Scaffold(
         appBar: AppBar(
           title: Text('River Aare in Olten'),
+          backgroundColor: Colors.blue.shade900,
         ),
 
         body: Center(
@@ -95,28 +97,28 @@ class TempAarState extends State<TempAar> {
                   children: [
                     Card(
                       child: ListTile(
-                        leading: const Icon(Icons.opacity,size: iconSize),
+                        leading: const Icon(Icons.opacity,size: iconSize, color: iconColor),
                         title: Text('Water Temperature',textScaleFactor: titleScale,),
                         subtitle: Text(reading.data.celsius1.toStringAsFixed(1)+' °C',textScaleFactor: subtitleScale),
                       )
                     ),
                     Card(
                       child: ListTile(
-                        leading: const Icon(Icons.wb_cloudy,size:iconSize),
+                        leading: const Icon(Icons.wb_cloudy,size:iconSize, color: iconColor),
                         title: Text('Air Temperature',textScaleFactor: titleScale,),
                         subtitle: Text(reading.data.celsius2.toStringAsFixed(1)+' °C',textScaleFactor: subtitleScale),
                       )
                     ),
                     if (showVoltage) Card(
                       child: ListTile(
-                        leading: const Icon(Icons.battery_std,size:iconSize),
+                        leading: const Icon(Icons.battery_std,size:iconSize, color: iconColor),
                         title: Text('Battery Voltage',textScaleFactor: titleScale,),
                         subtitle: Text(reading.data.volt.toStringAsFixed(2) + ' V',textScaleFactor: subtitleScale,),
                       )
                     ),
                     Spacer(flex:1),
                     ListTile(
-                        leading: const Icon(Icons.watch_later,size:iconSize),
+                        leading: const Icon(Icons.watch_later,size:iconSize, color: iconColor),
                         title: Text('Last Measurement',textScaleFactor: titleScale,),
                         subtitle: Text(DateFormat("H:mm:ss d.M.yyyy").format(reading.data.time.toLocal()),textScaleFactor: 1.2),
                         onLongPress: () {
